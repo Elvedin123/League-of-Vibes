22fadf5249eaac863d9b3c0480dd5f67bee9b681
elvedin123 <elvedinc97@gmail.com>
2022-02-24 19:43:36 -0500

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_24_234146) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_25_004134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "teams", force: :cascade do |t|
+    t.string "champ1_img"
+    t.string "champ2_img"
+    t.string "champ3_img"
+    t.string "champ4_img"
+    t.string "champ5_img"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_teams_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -25,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_24_234146) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "teams", "users"
 end
