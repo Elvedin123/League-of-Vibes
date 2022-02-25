32789662393eabbc5eae03798791dcb45fe33f6a
elvedin123 <elvedinc97@gmail.com>
2022-02-24 20:00:45 -0500

    added more validates to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::API
   has_secure_password
 
-
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 6 }
 
 end
