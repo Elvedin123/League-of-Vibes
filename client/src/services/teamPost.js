7b266975853ca3a6795fae0ee22b7e9936befe2e
elvedin123 <elvedinc97@gmail.com>
2022-02-25 20:35:29 -0500

    created getting one team method

--- client/src/services/teamPost.js
+++ client/src/services/teamPost.js
@@ -4,3 +4,7 @@ export const getAllTeams = async () => {
   const res = await backendUrl.get('/teams')
   return res.data
 }
+export const getOneTeam = async (team_id) => {
+  const res = await backendUrl.get(`/teams/${team_id}`)
+  return res.data
+}
