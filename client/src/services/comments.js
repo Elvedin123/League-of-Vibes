29ed73c8dc0b9128d55e64cd54c541eb4ceb7f01
elvedin123 <elvedinc97@gmail.com>
2022-02-25 20:43:19 -0500

    created getteamcomments and create comment functions

--- client/src/services/comments.js
+++ client/src/services/comments.js
@@ -4,3 +4,8 @@ export const getTeamComments = async (team_id) => {
   const res = await backendUrl.get(`/teams/${team_id}/comments`)
   return res.data
 }
+
+export const createComment = async (team_id, commentData) => {
+  const res = await backendUrl.post(`/teams/${team_id}/comments`, { comment: commentData })
+  return res.data
+}
