d23446a6b4b8496677d227a019f0c728f2d58469
elvedin123 <elvedinc97@gmail.com>
2022-02-25 21:12:13 -0500

    created component folder and working on sign up

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,9 @@
+import { userState } from 'react'
+import { signupUser } from '../services/user.js'
+import { useNavigate } from 'react-router-dom'
+
+export default function Signup() {
+  return (
+    <div>Signup</div>
+  )
+}
