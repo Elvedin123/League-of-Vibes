068cc4b0157b13ecacf5aef321bb8a0d7bc61034
elvedin123 <elvedinc97@gmail.com>
2022-02-25 20:38:36 -0500

    created update team and delete team methods

--- client/src/services/teamPost.js
+++ client/src/services/teamPost.js
@@ -4,7 +4,18 @@ export const getAllTeams = async () => {
   const res = await backendUrl.get('/teams')
   return res.data
 }
+
 export const getOneTeam = async (team_id) => {
   const res = await backendUrl.get(`/teams/${team_id}`)
+  return res.data
+}
+
+export const updateTeam = async (team_id, teamData) => {
+  const res = await backendUrl.put(`/teams/${team_id}`, { team: teamData })
+  return res.data
+}
+
+export const deleteTeam = async (team_id) => {
+  const res = await backendUrl.delete(`/teams/${team_id}`)
   return res.data
 }
