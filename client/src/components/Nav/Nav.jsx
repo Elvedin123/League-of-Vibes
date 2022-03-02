70a8e2cb58e060a881aa9d5334f55aaddc07f8dc
elvedin123 <elvedinc97@gmail.com>
2022-03-02 02:18:51 -0500

    finished auth nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,20 +1,48 @@
 import { Link } from "react-router-dom"
 import navcss from "./Nav.module.css"
-
+import { BiLogOut } from 'react-icons/bi'
 export default function Nav(props) {
   const { logout, currentUser } = props
   // console.log(currentUser)
 
   return (
-    <div>
+    <div className={navcss.auth_login_nav}>
       {currentUser ?
-        <>
-          <Link to='/home'><h1>League of Vibes</h1></Link>
-          <h3>Welcome, {currentUser.username}</h3>
-          <Link to='/create'>Create a Team</Link>
-          <Link to='/profile'>Profile</Link>
-          <button onClick={logout} >Logout</button>
-        </>
+        <nav>
+          <div className={navcss.maincontainer}>
+            <div
+              className={navcss.titlecontainer}>
+              <h1 >
+                <Link
+                  className={navcss.authtitle}
+                  to='/home'>League of Vibes
+                </Link>
+              </h1>
+              <h3 className=
+                {navcss.welcome}>
+                Welcome,
+                {currentUser.username}
+              </h3>
+            </div>
+            <div className={navcss.navlink}>
+              <Link
+                className={navcss.create}
+                to='/create'>
+                Create a Team
+              </Link>
+              <Link
+                className={navcss.profile}
+                to='/profile'>
+                Profile
+              </Link>
+              <a className={navcss.logout}
+                href="http://localhost:3001/"
+                onClick={logout}>
+                <BiLogOut />
+              </a>
+            </div>
+          </div>
+        </nav>
         :
         <>
           <nav className={navcss.preauth_login_nav}>
