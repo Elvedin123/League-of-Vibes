90ef840eaeda55d5225c824b6a189fb49421d671
elvedin123 <elvedinc97@gmail.com>
2022-03-02 09:24:13 -0500

    ur;

--- client/src/services/backendAPI.js
+++ client/src/services/backendAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseUrl = 'https://league-of-vibes-backend.herokuapp.com/'
+const baseUrl = process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://league-of-vibes-backend.herokuapp.com/' : 'http://localhost:3000'
 
 export const backendUrl = axios.create({
   baseURL: baseUrl
